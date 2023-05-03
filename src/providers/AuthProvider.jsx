2f540131cf0879b3b19362b87bb21723dd7ea9ce
Tanvir Ahamed Sowmik <sowmik265@gmail.com>
2023-05-03 10:06:52 +0600

    firebase environment variale done

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,8 +1,10 @@
 import React, { createContext } from 'react';
+import {getAuth} from "firebase/auth";
+import app from '../firebase/firebase.config';
 
-const AuthContext = createContext(null);
+export const AuthContext = createContext(null);
 
-
+const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
     const authInfo = {}
     return (
