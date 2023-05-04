d15726e3784633bf0f979100882cd7d2025bf869
Tanvir Ahamed Sowmik <sowmik265@gmail.com>
2023-05-04 15:38:22 +0600

    toast added

--- src/components/ChefDetails.jsx
+++ src/components/ChefDetails.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const ChefDetails = () => {
 
@@ -15,60 +17,73 @@ const ChefDetails = () => {
 
     // console.log(chef)
     const { chefPicture, chefName, numberOfRecipes, likes, chefBio, yearsOfExperience, recipes1, recipes2, recipes3, ingredients1, ingredients2, ingredients3, cookingMethod1, cookingMethod2, cookingMethod3, ratings1, ratings2, ratings3 } = chef;
+
+    const [buttonClicked, setButtonClicked] = useState(false);
+    const handleButtonClick = () => {
+        toast.success("Added to Favorite!");
+        setButtonClicked(true);
+    };
+
+
     return (
         <div className='mt-10 m-4'>
             <div className="card lg:card-side bg-base-100 shadow-xl mx-auto">
                 <figure className='w-full md:w-1/3'><img src={chefPicture} alt="Album" /></figure>
                 <div className="card-body w-full md:w-2/3 ">
-                    <h2 className="card-title">{chefName}</h2>
-                    <p>Number of Recipes : {numberOfRecipes}</p>
-                    <p>About : {chefBio}</p>
-                    <p>Likes : {likes}</p>
-                    <p>Experience : {yearsOfExperience}</p>
+                    <h2 className="card-title text-xl font-extrabold text-green-700">{chefName}</h2>
+                    <p className='font-extrabold text-green-700'>Number of Recipes : <span className='font-semibold'>   {numberOfRecipes}</span> </p>
+                    <p className="card-title text-xl font-extrabold text-green-700">About-  <br></br> <span className='font-semibold'> {chefBio}</span> </p>
+                    <p className="card-title text-xl font-extrabold text-green-700">Likes : <span className='font-semibold'> {likes}</span> </p>
+                    <p className="card-title text-xl font-extrabold text-green-700">Experience : <span className='font-semibold'> {yearsOfExperience}</span></p>
 
                 </div>
             </div>
 
-            <div className='flex flex-col md:flex-row mx-auto justify-between mt-10'>
+            <div className='font-extrabold  text-5xl text-lime-600 drop-shadow-2xl text-center mt-10'>
+                Recipes of {chefName} !!
+            </div>
+
+            <div className='flex flex-col md:flex-row mx-auto justify-between items-center mt-10'>
 
 
 
-                <div className="card w-96 text-black font-bold">
+                <div className="card w-96 text-black font-bold border border-yellow-400">
                     <div className="card-body">
-                        <h2 className="card-title">{recipes1}</h2>
-                        <p>Ingredients : {ingredients1}</p>
-                        <p>Cooking Method: {cookingMethod1} </p>
-                        <p>Rating : {ratings1}</p>
+                        <h2 className="card-title text-red-400 font-extrabold"> <span>{recipes1}</span></h2>
+                        <p className=' text-cyan-500'>Ingredients : <span className='text-bold text-violet-800'>{ingredients1}</span></p>
+                        <p className=' text-cyan-500'>Cooking Method: <span className='text-bold text-violet-800'>{cookingMethod1}</span></p>
+                        <p className=' text-cyan-500'>Rating : <span className='text-bold text-violet-800'>{ratings1}</span></p>
                         <div className="card-actions justify-end">
-                            <button className="btn">Favorite</button>
+                            <button onClick={handleButtonClick} disabled={buttonClicked} className="btn bg-yellow-400 border-none hover:bg-blue-700">Favorite</button>
                         </div>
                     </div>
                 </div>
 
-                <div className="card w-96 text-black font-bold">
+                <div className="card w-96 text-black font-bold border border-yellow-400">
                     <div className="card-body">
-                        <h2 className="card-title">{recipes2}</h2>
-                        <p>Ingredients : {ingredients2}</p>
-                        <p>Cooking Method: {cookingMethod2} </p>
-                        <p>Rating : {ratings2}</p>
+                        <h2 className="card-title text-red-400 font-extrabold"> <span>{recipes2}</span></h2>
+                        <p className=' text-cyan-500'>Ingredients : <span className='text-bold text-violet-800'>{ingredients2}</span></p>
+                        <p className=' text-cyan-500'>Cooking Method: <span className='text-bold text-violet-800'>{cookingMethod2}</span></p>
+                        <p className=' text-cyan-500'>Rating : <span className='text-bold text-violet-800'>{ratings2}</span></p>
                         <div className="card-actions justify-end">
-                            <button className="btn">Favorite</button>
+                            <button onClick={handleButtonClick} disabled={buttonClicked} className="btn bg-yellow-400 border-none hover:bg-blue-700">Favorite</button>
                         </div>
                     </div>
                 </div>
 
-                <div className="card w-96 text-black font-bold">
+                <div className="card w-96 text-black font-bold border border-yellow-400">
                     <div className="card-body">
-                        <h2 className="card-title">{recipes3}</h2>
-                        <p>Ingredients : {ingredients3}</p>
-                        <p>Cooking Method: {cookingMethod3} </p>
-                        <p>Rating : {ratings3}</p>
+                        <h2 className="card-title text-red-400 font-extrabold"> <span>{recipes3}</span></h2>
+                        <p className=' text-cyan-500'>Ingredients : <span className='text-bold text-violet-800'>{ingredients3}</span></p>
+                        <p className=' text-cyan-500'>Cooking Method: <span className='text-bold text-violet-800'>{cookingMethod3}</span></p>
+                        <p className=' text-cyan-500'>Rating : <span className='text-bold text-violet-800'>{ratings3}</span></p>
                         <div className="card-actions justify-end">
-                            <button className="btn">Favorite</button>
+                            <button onClick={handleButtonClick} disabled={buttonClicked} className="btn bg-yellow-400 border-none hover:bg-blue-700">Favorite</button>
                         </div>
                     </div>
                 </div>
             </div>
+            <ToastContainer />
 
         </div>
     );
--- src/components/HomePageDesign/TextImg.jsx
+++ src/components/HomePageDesign/TextImg.jsx
@@ -14,7 +14,7 @@ const TextImg = () => {
                     <Lottie animationData={Animation}></Lottie>
                 </div>
                 <div class="md:w-1/2 md:pl-8">
-                    <h2 class="text-2xl md:text-3xl font-bold mb-4 text-blue-700">Bored or Hungry?</h2>
+                    <h2 class="text-2xl md:text-3xl font-bold mb-4 text-blue-700">Bored and Hungry?</h2>
                     <p class="text-pink-700 mb-4 font-serif font-semi-bold">Quick recipes are perfect for busy individuals who are short on time but still want to enjoy a delicious meal. These recipes typically require minimal ingredients and can be prepared in under 30 minutes. Quick recipes are often simple, yet satisfying, and can range from salads and sandwiches to stir-fries and pastas. Whether you're looking to whip up a quick lunch or dinner, quick recipes are a convenient and tasty option.</p>
                     <a href="#" class="bg-yellow-400 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">View Quick Recipes</a>
                 </div>
